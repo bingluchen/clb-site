619fc252d3472e4b461169a9b12a368d709fcf07
陈路炳 <lubingchen@anjuke.com>
2014-08-19 18:11:28 +0800

    github

---
+++ localShare/src/com/common/BaseAction.java
@@ -0,0 +1,10 @@
+package com.common;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@SuppressWarnings("serial")
+public class BaseAction {
+	
+}
---
+++ localShare/src/com/index/action/CreateIndex.java
@@ -0,0 +1,5 @@
+package com.index.action;
+
+public class CreateIndex {
+
+}
