f3c6cdd27e729052577ba9adac6f09c2bd769967
陈路炳 <lubingchen@anjuke.com>
2014-08-26 17:14:33 +0800

    add

--- localShare/WebRoot/WEB-INF/share/shareUrl.jsp
+++ localShare/WebRoot/WEB-INF/share/shareUrl.jsp
@@ -1,5 +1,15 @@
 <%@page contentType="text/html; charset=utf-8"%>
+<%@ taglib prefix="s" uri="/struts-tags"%>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-	
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"  />
+		<title>I Want Share</title>
+		<link rel="stylesheet" href="/css/main.css" type="text/css"/>
+	</head>
+	<body>
+		<form action="share/shareMyFtp.action" method="post">
+			<input name="ftpDress" value=""/><input type="submit" value="分  享"/>
+		</form>
+	</body>
 </html>
--- localShare/src/com/share/action/AcceptShareAction.java
+++ localShare/src/com/share/action/AcceptShareAction.java
@@ -1,5 +1,6 @@
 package com.share.action;
 
+import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 
 import com.common.BaseAction;
@@ -7,6 +8,22 @@ import com.common.BaseAction;
 @Namespace("/share")
 public class AcceptShareAction extends BaseAction{
 	
+	private String ftpDress;
+	
+	@Action(value = "shareMyFtp")
+	public void shareFtp(){
+		System.out.println("QQQQQ"+ftpDress);
+	}
+
 	
 	
+	
+	
+	public String getFtpDress() {
+		return ftpDress;
+	}
+
+	public void setFtpDress(String ftpDress) {
+		this.ftpDress = ftpDress;
+	}
 }
